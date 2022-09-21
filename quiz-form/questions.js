98567ce5ff5395215c9481550efc6e3c7e744bd3
beatriznaufel <mariabncarvalho@gmail.com>
2022-09-20 22:46:04 -0300

    Day 14 done

---
+++ quiz-form/questions.js
@@ -0,0 +1,59 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which language runs in a web browser?",
+    answer: "Javascript",
+    options: [
+      "Java",
+      "C",
+      "Javascript",
+      "Python"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What year was Javascript launched?",
+    answer: "1995",
+    options: [
+      "1996",
+      "1995",
+      "1994",
+      "none of the above"
+    ]
+
+  },
+
+];
