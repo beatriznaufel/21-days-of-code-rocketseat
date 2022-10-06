a63744d82ab8e1b72e6feee7885849c8f656990c
beatriznaufel <mariabncarvalho@gmail.com>
2022-10-06 17:09:11 -0300

    Day 20 done

---
+++ emoji-picker/script.js
@@ -0,0 +1,29 @@
+const container = document.querySelector('.emoji-container')
+const copyBtn = document.querySelector('.copy-btn')
+const emojiBtn = document.querySelector('.emoji-btn')
+
+let picker = new EmojiButton({
+  position: 'right-start'
+})
+
+picker.on('emoji', (emoji) => {
+  container.innerHTML = emoji
+})
+
+emojiBtn.addEventListener('click', () => {
+  if(picker.pickerVisible) {
+    picker.hidePicker()
+  } else {
+    picker.showPicker(container)
+  }
+})
+
+copyBtn.addEventListener('click', async () => {
+  if(!container.innerHTML) {
+    alert('Choose the emoji.')
+    return
+  }
+  
+  await window.navigator.clipboard.writeText(container.innerHTML)
+  alert('Copied!')
+})
